65e2e454a43f5911caf9b0466209e3dd0546954a
fernandocoba <fernandocoba@gmail.com>
2020-04-02 20:39:13 -0300

    add import.sql for insert elements into product table in HB data base

--- src/main/java/com/springboot/microservice/microservices_product/controllers/ProductController.java
+++ src/main/java/com/springboot/microservice/microservices_product/controllers/ProductController.java
@@ -1,12 +1,12 @@
 package com.springboot.microservice.microservices_product.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,15 +21,29 @@ public class ProductController {
 	@Autowired
 	ProductService productService;
 
-	@GetMapping(value = "getAllProduct")
-	public ResponseEntity<Response> obtainAllProduct() {
-		List<Product> product = new ArrayList<>();
+	@GetMapping(value = "/obtainInformationAllProducts")
+	public ResponseEntity<Response> obtainAllProducts() {
 
-		product = productService.findAllProducts();
+		List<Product> product = productService.findAllProducts();
+
+		return product.isEmpty()
+
+				? new ResponseEntity<>(new Response(String.valueOf(HttpStatus.OK.value()), "Not Data Found", null),
+						HttpStatus.BAD_REQUEST)
+
+				: new ResponseEntity<>(new Response(String.valueOf(HttpStatus.OK), null, product), HttpStatus.OK);
+	}
+
+	@GetMapping(value = "obtainProductInformation/{productId}")
+	public ResponseEntity<Response> obtainProductBy(@PathVariable Long productId) {
+
+		Product product = productService.findProductById(productId);
 
 		return product == null
 
-				? new ResponseEntity<>(new Response(String.valueOf(HttpStatus.BAD_REQUEST), "Not Data Found", null),
+				? new ResponseEntity<>(
+						new Response(String.valueOf(HttpStatus.OK.value()),
+								"Not Data Found for this id : ".concat(String.valueOf(productId)), null),
 						HttpStatus.BAD_REQUEST)
 
 				: new ResponseEntity<>(new Response(String.valueOf(HttpStatus.OK), null, product), HttpStatus.OK);
--- src/main/java/com/springboot/microservice/microservices_product/model/entity/Product.java
+++ src/main/java/com/springboot/microservice/microservices_product/model/entity/Product.java
@@ -3,6 +3,7 @@ package com.springboot.microservice.microservices_product.model.entity;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,7 +24,9 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "PRODUCTNAME")
 	private String productName;
+	@Column(name = "CRATIONDATE")
 	@Temporal(TemporalType.DATE)
 	private Date crationDate;
 	private BigDecimal price;
